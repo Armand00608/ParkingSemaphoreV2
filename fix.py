ede3485b150fee51b4fea17948d029fe07e22e89
Armand Bermendora <polarmand06@gmail.com>
2026-03-19 09:47:24 +0100

    refactor: Remove unused classes and files from the parking simulation

--- fix.py
+++
@@ -1,3 +0,0 @@
-import codecs 
-with codecs.open('c:/IUT/TP/s4/r4.01_architect_logicielle/Tp2/ParkingSemaphoreV2/src/Parking/ihm/PanelDehors.java', 'w', encoding='utf-8') as f: 
-    f.write(content) 
